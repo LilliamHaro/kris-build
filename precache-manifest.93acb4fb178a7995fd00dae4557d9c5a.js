f89048cc403cd6e42a65f1b5e435e6c7585a249d
LilliamHaro <qualia950@gmail.com>
2019-02-05 10:28:38 -0500

    Updates

---
+++ precache-manifest.93acb4fb178a7995fd00dae4557d9c5a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "d795585bd178b944091f0db5658786a0",
+    "url": "/kris-build/static/media/banner-texto.d795585b.png"
+  },
+  {
+    "revision": "a99f0bded1ad6cf15ba4",
+    "url": "/kris-build/static/css/main.bcc2c59d.chunk.css"
+  },
+  {
+    "revision": "321532eeb89e8e8b1faa",
+    "url": "/kris-build/static/js/1.321532ee.chunk.js"
+  },
+  {
+    "revision": "66f7083da7855f47dbb5",
+    "url": "/kris-build/static/js/runtime~main.66f7083d.js"
+  },
+  {
+    "revision": "851cbbf1095f4f2b9cf4a4f6960385e4",
+    "url": "/kris-build/static/media/banner.851cbbf1.png"
+  },
+  {
+    "revision": "1dcf61689e88a3bd016365debd35da3b",
+    "url": "/kris-build/static/media/kris-claro.1dcf6168.png"
+  },
+  {
+    "revision": "a99f0bded1ad6cf15ba4",
+    "url": "/kris-build/static/js/main.a99f0bde.chunk.js"
+  },
+  {
+    "revision": "95f2e89eee1dabdf349b88e5eafce19f",
+    "url": "/kris-build/static/media/descubre.95f2e89e.png"
+  },
+  {
+    "revision": "3fe59f9659f061de0f4959f8e58da6af",
+    "url": "/kris-build/static/media/como-participar.3fe59f96.png"
+  },
+  {
+    "revision": "933d03a75ce2c69696867c27a5fe03eb",
+    "url": "/kris-build/static/media/canjea-premio.933d03a7.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kris-build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ade5996ceab7579cad6eed367e11b668",
+    "url": "/kris-build/static/media/bg-body.ade5996c.jpg"
+  },
+  {
+    "revision": "8de396173b6849c098515768cf632776",
+    "url": "/kris-build/index.html"
+  }
+];
